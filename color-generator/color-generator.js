235925865fef00ec62e2f001518b3650798a33bb
julieprunaret <j.prunaret@gmail.com>
2023-08-19 19:02:45 +0200

    add new exercize

---
+++ color-generator/color-generator.js
@@ -0,0 +1,15 @@
+const colorContainer = document.getElementById("color-container");
+
+const getColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    const color = `rgb(${r}, ${g}, ${b})`;
+
+    document.body.style.background = color;
+    colorContainer.innerText= color;
+}
+
+setInterval(getColor, 3000)
+
